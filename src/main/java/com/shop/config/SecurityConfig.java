4731410f804afb74aca20417fc3e3ebe7232326e
Nortiz <nortiz1349@gmail.com>
2022-07-06 11:42:04 +0900

    chapter 4 updated

--- src/main/java/com/shop/config/SecurityConfig.java
+++ src/main/java/com/shop/config/SecurityConfig.java
@@ -39,14 +39,14 @@ public class SecurityConfig {
                 .csrfTokenRepository(CookieCsrfTokenRepository
                         .withHttpOnlyFalse());
 
-        /*http.authorizeRequests()
+        http.authorizeRequests()
                 .mvcMatchers("/css/**","/js/**","/img/**","/assets/**","/pages/**","/scss/**").permitAll()
                 .mvcMatchers("/", "/members/**", "/item/**", "/images/**").permitAll()
                 .mvcMatchers("/admin/**").hasRole("ADMIN")
                 .anyRequest().authenticated();
 
         http.exceptionHandling()
-                .authenticationEntryPoint(new CustomAuthenicationEntryPoint());*/
+                .authenticationEntryPoint(new CustomAuthenticationEntryPoint());
 
         return http.build();
     }
@@ -57,52 +57,3 @@ public class SecurityConfig {
     }
 
 }
-
-
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-//
-////  WebSecurityConfigurerAdapter 를 상속 받는 클래스에 @EnableWebSecurity 어노테이션을 선언하면
-////  SpringSecurityFilterChain 이 자동으로 포함된다. WebSecurityConfigurerAdapter 를 상속 받아서
-////  메서드 오버라이딩을 통해 보안 설정을 커스터마이징 할 수 있다.
-////  deprecated 이슈 확인 필요
-//
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfig extends WebSecurityConfigurerAdapter {
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception { // http 요청에 대한 보안 설정
-//
-//        http
-//                .csrf()
-//                .csrfTokenRepository(CookieCsrfTokenRepository
-//                        .withHttpOnlyFalse());
-//
-//        http
-//                .authorizeRequests()
-//                .antMatchers("/h2-console/*")
-//                .permitAll();
-//
-////        h2 db 접근시
-////        http
-////                .csrf()
-////                .disable();
-//
-//        http
-//                .headers()
-//                .frameOptions()
-//                .disable();
-//    }
-//
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {  // 비밀번호 암호화
-//        return new BCryptPasswordEncoder();
-//    }
-//}
